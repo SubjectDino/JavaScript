923927c2c2e337f86c5bbfadbfa7cb6cf885a674
Ethan <ethancr8n@gmail.com>
2024-08-11 21:52:39 -0700

    Update

---
+++ Project2_functions/JS/main.js
@@ -0,0 +1,12 @@
+//Variables
+
+var A = "Variable Part 1", B = "Variable Part 2"
+
+document.getElementById(A).style.color = "red";
+
+function myFunction()
+{
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence;
+}
