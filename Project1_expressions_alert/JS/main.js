826d7ecd2e2cfd5600eab6f3dc7fd82ea1f302da
Ethan <ethancr8n@gmail.com>
2024-08-11 21:22:19 -0700

    Update

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+// Variables
+
+var A = "String 1";
+var B = "\"String 2\"";
+var C = "String..."
+var crew = "A.R.C. team", leader = "Nick Cutter", PR = "Jenny Lewis";
+
+// Alert
+window.alert(A);
+
+// Documents
+
+document.write('Hello, world!');
+document.write(B);
+document.write(C
++"3");
+document.write(crew)
+document.write(3+3);
