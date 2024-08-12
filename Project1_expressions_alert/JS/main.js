ede1a7c406cacf43238ca2853990e2004b039091
Ethan <ethancr8n@gmail.com>
2024-08-12 14:47:56 -0700

    Update

--- Project1_expressions_alert/JS/main.js
+++ Project1_expressions_alert/JS/main.js
@@ -4,11 +4,14 @@ var A = "String 1";
 var B = "\"String 2\"";
 var C = "String..."
 var crew = "A.R.C. team", leader = "Nick Cutter", PR = "Jenny Lewis";
+var Sent1 = "This is the beginning of a string"
+var Sent2 = "and this is the end of a string."
 
-// Alert
+// Alert with text displaying var A
+
 window.alert(A);
 
-// Documents
+// WRiting out variables, or misc words
 
 document.write('Hello, world!');
 document.write(B);
@@ -16,3 +19,5 @@ document.write(C
 +"3");
 document.write(crew)
 document.write(3+3);
+document.write(Sent1
++Sent2);
