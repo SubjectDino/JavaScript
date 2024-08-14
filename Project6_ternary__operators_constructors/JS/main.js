3f33b3010640c25298c2cc9394d79c82f2fc9030
Ethan <ethancr8n@gmail.com>
2024-08-14 12:54:34 -0700

    Update

--- Project6_ternary__operators_constructors/JS/main.js
+++ Project6_ternary__operators_constructors/JS/main.js
@@ -1,7 +1,7 @@
 function Ride_Function()
 {
-    var Height, Can_ride;
-    Height = document.getElementById("Height").value;
-    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+    var Height, Can_ride; // Sets up variable
+    Height = document.getElementById("Height").value; //Height
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough"; // Can ride variable
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride."; // Shows text
 }
